071b496ffe2f92b5b205856e96cdae9b5604d538
Wei-HaiMing <ezedito@gmail.com>
2025-10-19 14:42:48 -0700

    adjusted pathing for req params

--- src/main/java/com/grouptwelve/grouptwelveBE/controller/AuthController.java
+++ src/main/java/com/grouptwelve/grouptwelveBE/controller/AuthController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,7 +30,7 @@ public class AuthController {
      */
     @GetMapping("/start")
     public String authStart(
-            @RequestParam(required = false) String redirect_ip,
+            @RequestParam(value = "redirect_ip", required = false) String redirect_ip,
             HttpServletRequest request) {
         
         // Store the redirect IP in session so it can be retrieved after OAuth callback
@@ -40,8 +39,8 @@ public class AuthController {
             session.setAttribute("redirect_ip", redirect_ip);
             System.out.println("Stored redirect_ip in session: " + redirect_ip);
         }
-        
-        return "https://bettingprojheroku-0f16500feb98.herokuapp.com/oauth2/authorization/github";
+
+        return "{\"url\": \"https://bettingprojheroku-0f16500feb98.herokuapp.com/oauth2/authorization/github\"}";
     }
 
     // @GetMapping("/callback")
